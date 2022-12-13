caa1ff918721ae4fa4e58bfbb27c6e166bf599ce
yousan <ka1357amnbpdr@gmail.com>
2022-12-13 23:10:42 +0900

    fix: file

---
+++ Assets/_Project/Scripts/LoopMove.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Cysharp.Threading.Tasks;
+using UniRx;
+using UnityEngine;
+
+namespace _Project
+{
+    public class LoopMove : MonoBehaviour
+    {
+        private const float RightMoveTime = 5f;
+        private const float LeftMoveTime = 7f;
+
+        private void Start()
+        {
+            const float loopTime = RightMoveTime + LeftMoveTime;
+            Observable.Timer(TimeSpan.Zero, TimeSpan.FromSeconds(loopTime))
+                .Subscribe(async _ =>
+                {
+                    await Move.MoveOnTime(transform, new Vector3(0.05f, 0f, 0f), RightMoveTime, this.GetCancellationTokenOnDestroy());
+                    await Move.MoveOnTime(transform, new Vector3(-0.05f, 0f, 0f), LeftMoveTime, this.GetCancellationTokenOnDestroy());
+                }).AddTo(this);
+        }
+    }
+}
---
+++ Assets/_Project/Scripts/Move.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Threading;
+using Cysharp.Threading.Tasks;
+using UniRx;
+using UnityEngine;
+
+namespace _Project
+{
+    public static class Move
+    {
+        private const float MoveTimePerTick = 0.1f;
+        private const float MoveValuePerTick = 0.1f;
+
+        public static async UniTask MoveOnTime(Transform transform, Vector3 moveValue, float moveTime, CancellationToken token)
+        {
+            var moveDispose = Observable.Timer(TimeSpan.Zero, TimeSpan.FromSeconds(MoveTimePerTick))
+                .Subscribe(_ => { transform.Translate(moveValue * (MoveTimePerTick / moveTime)); });
+
+            try
+            {
+                await UniTask.Delay(TimeSpan.FromSeconds(moveTime), cancellationToken: token);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                moveDispose.Dispose();
+            }
+
+            moveDispose.Dispose();
+        }
+    }
+}
